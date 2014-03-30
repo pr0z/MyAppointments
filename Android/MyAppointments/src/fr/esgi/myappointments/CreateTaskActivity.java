638823eb0874723dce87b843e57aa19b5023eb95
ThundeR78 <julienwetstein@gmail.com>
2014-03-30 23:05:24 +0200

    Change business objects

--- Android/MyAppointments/src/fr/esgi/myappointments/CreateTaskActivity.java
+++ Android/MyAppointments/src/fr/esgi/myappointments/CreateTaskActivity.java
@@ -9,10 +9,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class CreateTaskActivity extends Activity {
 
